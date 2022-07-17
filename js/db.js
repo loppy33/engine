a2a8064c9849b50694971718c7b3ed3ddd2052ed
loppy33 <nikita.petrenko1406@gmail.com>
2022-07-17 14:54:06 +0300

    new commit

--- js/db.js
+++ js/db.js
@@ -1,10 +1,21 @@
 let db = `
-
 https://discord.gg/invite/anko
 --
 guild2.png
 --
 Anko
+&&
+https://discord.gg/invite/akame
+--
+guild6.gif
+--
+Akame
+&&
+https://discord.gg/Ycx2EuucRW
+--
+guild7.png
+--
+Loon
 &&
 https://discord.gg/aAQVuUpgba
 --
@@ -12,17 +23,19 @@ guild1.gif
 --
 Akity
 &&
+https://discord.gg/westa
+--
+guild8.gif
+--
+Westa
+&&
 https://discord.gg/Akcdva53xX
 --
 guild5.gif
 --
 Kyoto
-&&
-https://discord.gg/invite/akame
---
-guild6.gif
---
-Akame
 `;
 
 export {db} 
+
+
